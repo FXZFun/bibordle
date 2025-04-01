f242206a400d64ec0fa54ce20c3f0629d3c3fe3c
FXZFun <58504073+FXZFun@users.noreply.github.com>
2025-04-01 00:43:00 -0500

    refactor: remove unused files and scripts for improved clarity and performance
    And it came to pass, that the files of old were cast away, and the scripts that served no purpose were no more, that the path might be made clear and the performance increased.

--- compiler.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call google-closure-compiler C:\Users\rj\Documents\GitHub\bibordle\public\res\scripts\bibordle-unlimited.js --js_output_file C:\Users\rj\Documents\GitHub\bibordle\public\res\scripts\bibordle-unlimited.min.js
-call google-closure-compiler C:\Users\rj\Documents\GitHub\bibordle\public\res\scripts\bibordle.js --js_output_file C:\Users\rj\Documents\GitHub\bibordle\public\res\scripts\bibordle.min.js
-call csso C:\Users\rj\Documents\GitHub\bibordle\public\res\styles\main.css -o C:\Users\rj\Documents\GitHub\bibordle\public\res\styles\main.min.css
--- deploy.bat
+++
@@ -1,2 +0,0 @@
-call compiler.bat
-firebase deploy
